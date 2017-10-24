8547d05410d6e05df325782624a37540ad2496f2
Matthew Liu <ninja2koo@hotmail.com>
2017-10-24 12:22:11 -0400

    create user db, add user model and auth features

---
+++ app/models/user.rb
@@ -0,0 +1,38 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  username        :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
+class User < ApplicationRecord
+
+  validates :username, :session_token, presence: true, uniqueness: true
+  validates :password_digest, presence: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+
+  attr_reader :password
+
+  before_validation :ensure_session_token
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrupt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def ensure_session_token
+    self.session_token ||= SecureRandom::urlsafe_base64(16)
+    self.save!
+    self.session_token
+  end
+
+end
