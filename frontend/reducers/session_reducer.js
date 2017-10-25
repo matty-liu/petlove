b0fb18f79f382a2778ab477cc4c42682a845565d
Matthew Liu <ninja2koo@hotmail.com>
2017-10-25 00:16:33 -0400

    implement session actions, and session reducers

---
+++ frontend/reducers/session_reducer.js
@@ -0,0 +1,17 @@
+import { RECEIVE_CURRENT_USER } from '../actions/session_actions';
+
+const sessionReducer = ( state = {currentUser: null}, action ) => {
+  let newState;
+  switch (action.type) {
+
+    case RECEIVE_CURRENT_USER:
+      const user = action.user;
+      newState = { currentUser: action.user };
+      return newState;
+
+    default:
+      return state;
+  }
+};
+
+export default sessionsReducer
