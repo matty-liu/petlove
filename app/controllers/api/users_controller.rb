3f720b5d1edf9c459242dc07601f40793c4f2ebf
Matthew Liu <ninja2koo@hotmail.com>
2017-10-24 22:24:23 -0400

    fix routing bug on json response

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -2,10 +2,10 @@ class Api::UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-
     if @user.save
+      debugger
       login(@user)
-      render :show
+      render "api/users/show"
     else
       render json @user.errors.full_messages
     end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
 
   root to: 'static_pages#root'
 
-  # namespace :api, defaults: {format: :json} do
-  #   resources :users, only: [:create]
-  #   resource :sessions, only: [:create, :destroy]
-  # end
+  namespace :api, defaults: {format: :json} do
+    resources :users, only: [:create]
+    resource :sessions, only: [:create, :destroy]
+  end
 
 end
