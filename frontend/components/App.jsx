e84ded9eb4e5826c497f2a9f84a51a03005d60f1
Matthew Liu <ninja2koo@hotmail.com>
2017-10-25 01:05:26 -0400

    add components to be rendered in index

---
+++ frontend/components/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = () => (
+  <div>
+    <h1>petLove <3</h1>
+  </div>
+);
+
+export default App;
--- frontend/index.jsx
+++ frontend/index.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import configureStore from './store/store'
+import Root from './components/root'
+
 
 
 
@@ -10,5 +13,5 @@ document.addEventListener('DOMContentLoaded', () => {
   window.dispatch = store.dispatch;
 
   const root = document.getElementById('root');
-  ReactDOM.render(<h1>Welcome to petLove</h1>, root);
+  ReactDOM.render(<Root store={store}/>, root);
 });
