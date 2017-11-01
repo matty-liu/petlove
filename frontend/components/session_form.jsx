e6f1def36600e7054831fc6c743e11f50f4d32a5
Matthew Liu <ninja2koo@hotmail.com>
2017-11-01 08:15:38 -0400

    fix errors display in session form

--- frontend/components/session_form.jsx
+++ frontend/components/session_form.jsx
@@ -40,6 +40,7 @@ class SessionForm extends React.Component {
   render() {
     let errors;
     if (this.props.errors) {
+      debugger
       errors = this.props.errors.map( (error,index) => (
         <li key={index}>{error}</li>
       ))
