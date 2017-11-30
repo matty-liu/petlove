0b5f3db8ba4b9c5a28968ea067fac450f261a442
Matthew Liu <ninja2koo@hotmail.com>
2017-11-30 11:50:57 -0500

    add additional seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,8 @@
 
 User.delete_all
 
+demo = User.create!(username: "demo", password: "password")
+
 dog = User.create!(username: "Doggy", password: "password",
   sex: "Male", birthday: "", location: "New York",
   species: "Dog", age_range_min: 21, age_range_max: 100,
@@ -182,7 +184,6 @@ wile = User.create!(username: "WileE", password: "password",
   profile: "!",
   profile2: "you know how to catch that Road Runner")
 
-
   dog.image = "https://s3.us-east-2.amazonaws.com/petlove-pro/users/images/000/000/cutest-dog-breeds.jpg"
   cat.image = "https://s3.us-east-2.amazonaws.com/petlove-pro/users/images/000/000/cute-baby-kitty.jpg"
   pig.image = "https://s3.us-east-2.amazonaws.com/petlove-pro/users/images/000/000/cute-baby-animals-piggy.jpg"
@@ -190,7 +191,7 @@ wile = User.create!(username: "WileE", password: "password",
   gir.image = "https://s3.us-east-2.amazonaws.com/petlove-pro/users/images/000/000/cute-baby-giraf.jpg"
   ham.image = "https://s3.us-east-2.amazonaws.com/petlove-pro/users/images/000/000/hamster.jpg"
   wolf.image = "https://s3.us-east-2.amazonaws.com/petlove-pro/users/images/000/000/wolf-suit.jpg"
-  try.image = "https://s3.us-east-2.amazonaws.com/petlove-pro/users/images/000/000/trex.png"
+  # try.image = "https://s3.us-east-2.amazonaws.com/petlove-pro/users/images/000/000/trex.png"
   dog2.image = "https://s3.us-east-2.amazonaws.com/petlove-pro/users/images/000/000/lassie.jpg"
   goat1.image = "https://s3.us-east-2.amazonaws.com/petlove-pro/users/images/000/000/goat1.jpg"
   goat2.image = "https://s3.us-east-2.amazonaws.com/petlove-pro/users/images/000/000/goat2.jpg"
@@ -221,13 +222,124 @@ wile = User.create!(username: "WileE", password: "password",
   daffy.save!
   wile.save!
 
+dog3 = User.create!(username: "Doge", password: "password",
+  sex: "Male", birthday: "", location: "New York",
+  species: "Dog", age_range_min: 21, age_range_max: 100,
+  look_for: "a friend", look_gender: "does not matter",
+  size: "medium", status: "single", weight: "average",
+  orientation:"Single", compatibility_value: 60,
+  profile: "Love people, playing around, running around, eating, sleeping Looking for long walks on the beach
+  and someone to play with!", profile2: "If you are looking for someone to play catch with!")
+dog3.image = "https://s3.us-east-2.amazonaws.com/petlove-pro/users/images/000/000/seeds_images/doge.jpg"
+
+dog4 = User.create!(username: "Buddy", password: "password",
+  sex: "Male", birthday: "", location: "New York",
+  species: "Dog", age_range_min: 21, age_range_max: 100,
+  look_for: "a friend", look_gender: "does not matter",
+  size: "medium", status: "single", weight: "average",
+  orientation:"Single", compatibility_value: 72,
+  profile: "Love people, playing around, running around, eating, sleeping Looking for long walks on the beach
+  and someone to play with!", profile2: "If you are looking for someone to play catch with!")
+dog4.image = "https://s3.us-east-2.amazonaws.com/petlove-pro/users/images/000/000/seeds_images/Air-Bud-Director-Charles-Martin-Smith-Doesnt-Like.jpg"
+
+dog5 = User.create!(username: "Barkley Sir Charles", password: "password",
+  sex: "Male", birthday: "", location: "New York",
+  species: "Dog", age_range_min: 21, age_range_max: 100,
+  look_for: "a friend", look_gender: "does not matter",
+  size: "medium", status: "single", weight: "average",
+  orientation:"Single", compatibility_value: 41,
+  profile: "Love people, playing around, running around, eating, sleeping Looking for long walks on the beach
+  and someone to play with!", profile2: "If you are looking for someone to play catch with!")
+dog5.image = "https://s3.us-east-2.amazonaws.com/petlove-pro/users/images/000/000/seeds_images/sir-charles-barkley-icecream.jpg"
+
+dog6 = User.create!(username: "MenswareDog", password: "password",
+  sex: "Male", birthday: "", location: "New York",
+  species: "Dog", age_range_min: 21, age_range_max: 100,
+  look_for: "a friend", look_gender: "does not matter",
+  size: "medium", status: "single", weight: "average",
+  orientation:"Single", compatibility_value: 38,
+  profile: "Love people, playing around, running around, eating, sleeping Looking for long walks on the beach
+  and someone to play with!", profile2: "If you are looking for someone to play catch with!")
+dog6.image = "https://s3.us-east-2.amazonaws.com/petlove-pro/users/images/000/000/seeds_images/menswaredog.jpeg"
+
+dog7 = User.create!(username: "Beethoven", password: "password",
+  sex: "Male", birthday: "", location: "New York",
+  species: "Dog", age_range_min: 21, age_range_max: 100,
+  look_for: "a friend", look_gender: "does not matter",
+  size: "medium", status: "single", weight: "average",
+  orientation:"Single", compatibility_value: 50,
+  profile: "Love people, playing around, running around, eating, sleeping Looking for long walks on the beach
+  and someone to play with!", profile2: "If you are looking for someone to play catch with!")
+dog7.image = "https://s3.us-east-2.amazonaws.com/petlove-pro/users/images/000/000/seeds_images/beethoven+dog.jpg"
+
+dog8 = User.create!(username: "Tramp", password: "password",
+  sex: "Male", birthday: "", location: "New York",
+  species: "Dog", age_range_min: 21, age_range_max: 100,
+  look_for: "a friend", look_gender: "does not matter",
+  size: "medium", status: "single", weight: "average",
+  orientation:"Single", compatibility_value: 35,
+  profile: "Love people, playing around, running around, eating, sleeping Looking for long walks on the beach
+  and someone to play with!", profile2: "If you are looking for someone to play catch with!")
+dog8.image = "https://s3.us-east-2.amazonaws.com/petlove-pro/users/images/000/000/seeds_images/Tramp325.jpg"
+
+dog9 = User.create!(username: "Milo", password: "password",
+  sex: "Male", birthday: "", location: "New York",
+  species: "Dog", age_range_min: 21, age_range_max: 100,
+  look_for: "a friend", look_gender: "does not matter",
+  size: "medium", status: "single", weight: "average",
+  orientation:"Single", compatibility_value: 41,
+  profile: "Love people, playing around, running around, eating, sleeping Looking for long walks on the beach
+  and someone to play with!", profile2: "If you are looking for someone to play catch with!")
+dog9.image = "https://s3.us-east-2.amazonaws.com/petlove-pro/users/images/000/000/seeds_images/milo.jpg"
+
+dog10 = User.create!(username: "Bolt", password: "password",
+  sex: "Male", birthday: "", location: "New York",
+  species: "Dog", age_range_min: 21, age_range_max: 100,
+  look_for: "a friend", look_gender: "does not matter",
+  size: "medium", status: "single", weight: "average",
+  orientation:"Single", compatibility_value: 68,
+  profile: "Love people, playing around, running around, eating, sleeping Looking for long walks on the beach
+  and someone to play with!", profile2: "If you are looking for someone to play catch with!")
+dog10.image = "https://s3.us-east-2.amazonaws.com/petlove-pro/users/images/000/000/seeds_images/Bolt_profile.jpg"
+
+dog11 = User.create!(username: "Hooch", password: "password",
+  sex: "Male", birthday: "", location: "New York",
+  species: "Dog", age_range_min: 21, age_range_max: 100,
+  look_for: "a friend", look_gender: "does not matter",
+  size: "medium", status: "single", weight: "average",
+  orientation:"Single", compatibility_value: 51,
+  profile: "Love people, playing around, running around, eating, sleeping Looking for long walks on the beach
+  and someone to play with!", profile2: "If you are looking for someone to play catch with!")
+dog11.image = "https://s3.us-east-2.amazonaws.com/petlove-pro/users/images/000/000/seeds_images/hooch.jpg"
+
+dog12 = User.create!(username: "Slink", password: "password",
+  sex: "Male", birthday: "", location: "New York",
+  species: "Dog", age_range_min: 21, age_range_max: 100,
+  look_for: "a friend", look_gender: "does not matter",
+  size: "medium", status: "single", weight: "average",
+  orientation:"Single", compatibility_value: 24,
+  profile: "Love people, playing around, running around, eating, sleeping Looking for long walks on the beach
+  and someone to play with!", profile2: "If you are looking for someone to play catch with!")
+dog12.image = "https://s3.us-east-2.amazonaws.com/petlove-pro/users/images/000/000/seeds_images/Toy-Story-3-Slinky-Dog.jpg"
+
+
+dog3.save!
+dog4.save!
+dog5.save!
+dog6.save!
+dog7.save!
+dog8.save!
+dog9.save!
+dog10.save!
+dog11.save!
+dog12.save!
 
 # Question.create!(question: "Are you okay with interspecies dating?")
 # Question.create!(question: "Are you a morning person? Night person?")
 # Question.create!(question: "What would you prefer to do in your free time?")
 # Question.create!(question: "What do you think of vegetarians?")
 # Question.create!(question: "Does size matter?")
-#
+
 
 # Answer.delete_all
 #
@@ -250,7 +362,7 @@ wile = User.create!(username: "WileE", password: "password",
 # Answer.create(answer:"Yes", question_id: 5, compatibility: -10)
 # Answer.create(answer:"No", question_id: 5, compatibility: 10)
 # Answer.create(answer:"Indifferent", question_id: 5, compatibility: 5)
-# #
+#
 # Response.create(user_id: 16, answer_id:1)
 # Response.create(user_id: 17, answer_id:1)
 # Response.create(user_id: 20, answer_id:2)
