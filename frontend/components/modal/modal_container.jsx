5b4931440590a1a1cbf89285fdb9f48d221d0ca8
Matthew Liu <ninja2koo@hotmail.com>
2017-12-08 13:29:02 -0500

    add dispatch to props to modal container

---
+++ frontend/components/modal/modal_container.jsx
@@ -0,0 +1,22 @@
+import { connect } from 'react-redux';
+
+import Modal from './modal';
+import { openModal, closeModal } from '../../actions/modal_action'
+
+function mapStateToProps(state, ownProps) {
+
+  return {
+    modalOpen: state.ui.modal.modalOpen
+  }
+
+}
+
+function mapDispatchToProps(dispatch, ownProps) {
+
+  return {
+    closeModal: () => dispatch(closeModal()),
+  }
+
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Modal)
