f64c29ff06e33e10c43aa57a107b417b8953c3f5
Matthew Liu <ninja2koo@hotmail.com>
2018-01-18 17:57:29 -0500

    fix formatting

--- frontend/reducers/likes_reducer.js
+++ frontend/reducers/likes_reducer.js
@@ -1,7 +1,7 @@
 import { FETCH_USER_LIKES } from '../actions/likes_action';
 
 const likesReducer = ( state = {}, action ) => {
-
+  
   switch (action.type) {
     case FETCH_USER_LIKES:
       return Object.assign({}, state,action.likes)
