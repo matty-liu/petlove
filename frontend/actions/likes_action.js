b47aaabb1bd1fd2b72b01c404b0e1ee5a23c57e3
Matthew Liu <ninja2koo@hotmail.com>
2018-01-18 17:55:21 -0500

    fix likes action dispatch type name

--- frontend/actions/likes_action.js
+++ frontend/actions/likes_action.js
@@ -1,14 +1,14 @@
 import * as APIUtil from '../util/likes_api_util';
 
-export const FETCH_USER_LIKES = "FETCH_ALL_USERS"
+export const FETCH_USER_LIKES = "FETCH_USER_LIKES"
 
 export const fetchUserLikes = (likes) => ({
   type: FETCH_USER_LIKES,
   likes
 })
 
-export const getLikes = () => {
+export const getLikes = () => (dispatch) => {
   return APIUtil
-  .getLikes()
-  .then((likes) => dispatch(fetchUserLikes(likes)))
+    .getLikes()
+    .then((likes) => dispatch(fetchUserLikes(likes)))
 }
