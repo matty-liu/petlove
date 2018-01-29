25cca464481f9fd29258e9c8ca3e85235a67d3c1
Matthew Liu <ninja2koo@hotmail.com>
2018-01-29 09:57:17 -0500

    add notes

--- app/controllers/api/likes_controller.rb
+++ app/controllers/api/likes_controller.rb
@@ -10,11 +10,21 @@ class Api::LikesController < ApplicationController
   end
 
   def create
-    @like = Like.new(like_params)
-    if @like.save
-      render "api/likes/show"
-    else
-      render json: @like.errors.full_messages, status: 422
+    # find a user like
+    @like = Like.find_by(liker_id: current_user.id, likee_id: like_params)
+    if @like # if user like exist, update the like
+      if @like.update(like: !@like.like)
+        render "api/likes/show"
+      else
+        render json: @like.errors.full_messages, status: 422
+      end
+    else # if user like doesn't exist, create the like
+      @like = Like.new(liker_id: current_user.id, likee_id: like_params, like: true)
+      if @like.save
+        render "api/likes/show"
+      else
+        render json: @like.errors.full_messages, status: 422
+      end
     end
   end
 
