5ebbdc80160126fb46017a3a378e6822ce1be11c
Matthew Liu <ninja2koo@hotmail.com>
2018-01-29 10:36:48 -0500

    update formatting

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -38,8 +38,10 @@ export const login = (user) => (dispatch) => {
 }
 
 export const logout = () => (dispatch) => {
-  return APIUtil.logout().then(
-    () => dispatch(receiveCurrentUser(null)),
-    (error) => dispatch(receiveSessionErrors(error))
-  )
+  return APIUtil
+    .logout()
+    .then(
+      () => dispatch(receiveCurrentUser(null)),
+      (error) => dispatch(receiveSessionErrors(error))
+    )
 }
