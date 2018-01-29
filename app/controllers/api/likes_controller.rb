2698f26d39e7548ea81522eddd2841d54059d350
Matthew Liu <ninja2koo@hotmail.com>
2018-01-29 16:57:32 -0500

    remove extraneous code for routes that were not being used

--- app/controllers/api/likes_controller.rb
+++ app/controllers/api/likes_controller.rb
@@ -11,7 +11,7 @@ class Api::LikesController < ApplicationController
 
   def create
     # find a user like
-    @like = Like.find_by(liker_id: current_user.id, likee_id: like_params["likee_id"])
+    @like = Like.find_by(liker_id: current_user.id, likee_id: like_params[:likee_id])
     if @like # if user like exist, update the like
       if @like.update(like: !@like.like)
         render "api/likes/show"
@@ -19,7 +19,7 @@ class Api::LikesController < ApplicationController
         render json: @like.errors.full_messages, status: 422
       end
     else # if user like doesn't exist, create the like
-      @like = Like.new(liker_id: current_user.id, likee_id: like_params["likee_id"], like: true)
+      @like = Like.new(liker_id: current_user.id, likee_id: like_params[:likee_id], like: true)
       if @like.save
         render "api/likes/show"
       else
@@ -28,21 +28,12 @@ class Api::LikesController < ApplicationController
     end
   end
 
-  def update
-    @like = current_user.likees.find_by(likee_id: like_params)
-    if @like.update(like_params)
-      render "api/users/update"
+  def show
+    @like = Like.find_by(liker_id: current_user.id, likee_id: params[:id])
+    if @like
+      render "api/likes/show"
     else
-      render json: @like.errors.full_messages, status: 422
-    end
-  end
-
-  def destroy
-    @like = current_user.likees.find_by(likee_id: like_params)
-    if @like.delete
-      render "api/users/delete"
-    else
-      render json: @like.errors.full_messages, status: 422
+      render json: "like does not exist"
     end
   end
 
