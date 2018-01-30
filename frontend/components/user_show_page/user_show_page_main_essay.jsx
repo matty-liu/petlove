c3cc1c6ec4897f682ee2e61e3ccfb0e1381ef74e
Matthew Liu <ninja2koo@hotmail.com>
2018-01-30 13:40:53 -0500

    add missing title

--- frontend/components/user_show_page/user_show_page_main_essay.jsx
+++ frontend/components/user_show_page/user_show_page_main_essay.jsx
@@ -10,6 +10,7 @@ class UserShowPageMainEssay extends React.Component {
         <div className="profile-main-essay-summary">
           {this.props.profile}
         </div>
+        <h2 className="profile-main-essay-title">You should send me a message if...</h2>
         <div className="profile-main-essay-summary">
           {this.props.profile2}
         </div>
