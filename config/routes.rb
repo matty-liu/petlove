59a2abb0075c72218b7c71c8ed72a1473ceeda47
Matthew Liu <ninja2koo@hotmail.com>
2018-01-29 23:26:52 -0500

    added user show page route

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'static_pages#root'
 
   namespace :api, defaults: {format: :json} do
-    resources :users, only: [:index, :create, :update]
+    resources :users, only: [:index, :create, :update, :show]
     resource :sessions, only: [:create, :destroy]
     resource :responses, only: [:create]
     resources :likes, only: [:index, :create, :show]
