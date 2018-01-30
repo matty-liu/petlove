4fd9759a74c373e80b7307f8a49c18fa0d842c41
Matthew Liu <ninja2koo@hotmail.com>
2018-01-29 23:32:24 -0500

    add individual user fetch

--- frontend/reducers/users_reducer.js
+++ frontend/reducers/users_reducer.js
@@ -1,4 +1,4 @@
-import { RECEIVE_UPDATE_USER, FETCH_ALL_USERS } from '../actions/users_actions';
+import { RECEIVE_UPDATE_USER, FETCH_USER, FETCH_ALL_USERS } from '../actions/users_actions';
 // import { RECEIVE_CURRENT_USER } from '../actions/session_actions';
 
 const usersReducer = ( state = {}, action ) => {
@@ -9,6 +9,11 @@ const usersReducer = ( state = {}, action ) => {
 
     case FETCH_ALL_USERS:
       return Object.assign({},state,action.users);
+
+    case FETCH_USER:
+      user = action.users;
+      newState = Object.assign({}, state, action.users);
+      return newState;
 
     case RECEIVE_UPDATE_USER:
       user = action.user;
